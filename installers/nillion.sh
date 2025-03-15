608f4749e2777c4e6da6c7bb22c5601a96a3c78b
FL1NEE <70909941+FL1NEE@users.noreply.github.com>
2025-03-15 19:02:30 +0300

    Update nillion.sh

--- installers/nillion.sh
+++ installers/nillion.sh
@@ -23,248 +23,130 @@ curl -s https://raw.githubusercontent.com/noxuspace/cryptofortochka/main/logo_cl
 echo -e "${YELLOW}Выберите действие:${NC}"
 echo -e "${CYAN}1) Установка ноды${NC}"
 echo -e "${CYAN}2) Обновление ноды${NC}"
-echo -e "${CYAN}3) Изменение комиссии${NC}"
-echo -e "${CYAN}4) Удаление ноды${NC}"
-echo -e "${CYAN}5) Проверка логов (выход из логов CTRL+C)${NC}"
+echo -e "${CYAN}3) Просмотр логов${NC}"
+echo -e "${CYAN}4) Замена RPC${NC}"
+echo -e "${CYAN}5) Бекап ноды${NC}"
+echo -e "${CYAN}6) Удаление ноды${NC}"
 
 echo -e "${YELLOW}Введите номер:${NC} "
 read choice
 
 case $choice in
     1)
-        echo -e "${BLUE}Устанавливаем ноду Hemi...${NC}"
+        echo -e "${BLUE}Устанавливаем ноду Nillion...${NC}"
 
-        # Обновляем и устанавливаем необходимые пакеты
-        sudo apt update && sudo apt upgrade -y
-        sleep 1
+        # Обновление и установка необходимых компонентов
+        sudo apt update -y
+        sudo apt upgrade -y
 
-        # Проверка и установка tar, если его нет
-        if ! command -v tar &> /dev/null; then
-            sudo apt install tar -y
+        # Проверка наличия Docker
+        if ! command -v docker &> /dev/null; then
+            echo -e "${YELLOW}Docker не установлен. Устанавливаем Docker...${NC}"
+            sudo apt install docker.io -y
+            echo -e "${GREEN}Docker успешно установлен!${NC}"
+        else
+            DOCKER_VERSION=$(docker --version | grep -oP '\d+\.\d+\.\d+')
+            MIN_DOCKER_VERSION="27.2.0"
+            if [[ "$(printf '%s\n' "$MIN_DOCKER_VERSION" "$DOCKER_VERSION" | sort -V | head -n1)" != "$MIN_DOCKER_VERSION" ]]; then
+                echo -e "${YELLOW}Docker версии $DOCKER_VERSION ниже необходимой $MIN_DOCKER_VERSION. Обновляем Docker...${NC}"
+                sudo apt install --only-upgrade docker.io -y
+                echo -e "${GREEN}Docker обновлен до версии $(docker --version | grep -oP '\d+\.\d+\.\d+')!${NC}"
+            fi
         fi
 
-        # Установка бинарника
-        echo -e "${BLUE}Загружаем бинарник Hemi...${NC}"
-        curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v1.0.0/heminetwork_v1.0.0_linux_amd64.tar.gz
+        # Проверка наличия Docker Compose
+        if ! command -v docker-compose &> /dev/null; then
+            echo -e "${YELLOW}Docker Compose не установлен. Устанавливаем Docker Compose...${NC}"
+            sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+            echo -e "${GREEN}Docker Compose успешно установлен!${NC}"
+        fi
 
-        # Создание директории и извлечение бинарника
-        mkdir -p hemi-main
-        tar --strip-components=1 -xzvf heminetwork_v1.0.0_linux_amd64.tar.gz -C hemi-main
-        cd hemi-main
+        # Установка Accuser Image
+        docker pull nillion/verifier:v1.0.1
 
-        # Создание tBTC кошелька
-        ./keygen -secp256k1 -json  > popm-address.json
+        # Создание директории для Accuser
+        mkdir -p $HOME/nillion/verifier
 
-        # Вывод содержимого файла popm-address.json
-        echo -e "${RED}Сохраните эти данные в надежное место:${NC}"
-        cat popm-address.json
-        echo -e "${PURPLE}Ваш pubkey_hash — это ваш адрес, на который нужно отправить BTC.${NC}"
+        # Запуск контейнера для инициализации и регистрации Accuser
+        docker run -v $HOME/nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
 
-        echo -e "${YELLOW}Введите ваш приватный ключ от кошелька:${NC} "
-        read PRIV_KEY
-        echo -e "${YELLOW}Укажите желаемый размер комиссии (рекомендуем 1-2):${NC} "
-        read FEE
-
-        echo "POPM_BTC_PRIVKEY=$PRIV_KEY" > popmd.env
-        echo "POPM_STATIC_FEE=$FEE" >> popmd.env
-        echo "POPM_BFG_URL=wss://pop.hemi.network/v1/ws/public" >> popmd.env
-        echo "POPM_BTC_CHAIN_NAME=mainnet" >> popmd.env
-        sleep 1
-
-        # Определяем имя текущего пользователя и его домашнюю директорию
-USERNAME=$(whoami)
-
-if [ "$USERNAME" == "root" ]; then
-    HOME_DIR="/root"
-else
-    HOME_DIR="/home/$USERNAME"
-fi
-
-# Создаем или обновляем файл сервиса с использованием определенного имени пользователя и домашней директории
-cat <<EOT | sudo tee /etc/systemd/system/hemi-main.service > /dev/null
-[Unit]
-Description=PopMD Main Service
-After=network.target
-
-[Service]
-User=$USERNAME
-EnvironmentFile=$HOME_DIR/hemi-main/popmd.env
-ExecStart=$HOME_DIR/hemi-main/popmd
-WorkingDirectory=$HOME_DIR/hemi-main/
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-        # Обновление сервисов и включение hemi
-        sudo systemctl daemon-reload
-        sudo systemctl restart systemd-journald
-        sudo systemctl enable hemi-main
-        sleep 1
+        # Ожидание действия пользователя
+        echo -e "${YELLOW}Вернитесь к гайду и выполните шаги, начиная с пункта запроса токенов в кране. После выполнения 5-го пункта на платформе, вернитесь сюда и нажмите Enter${NC}"
+        read -p ""
 
         # Запуск ноды
-        sudo systemctl start hemi-main
+        docker run -d --name nillion -v $HOME/nillion/verifier:/var/tmp nillion/verifier:v1.0.1 verify --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com"
 
-        # Заключительный вывод
-        echo -e "${GREEN}Установка завершена и нода запущена!${NC}"
-
-        # Завершающий вывод
+        # Заключительное сообщение
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-        echo "sudo journalctl -u hemi-main -f"
+        echo -e "${YELLOW}Команда для проверки логов контейнера:${NC}"
+        echo "docker logs -f nillion"
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
         echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
         sleep 2
-        sudo journalctl -u hemi-main -f
+
+        # Запуск логов
+        docker logs -f nillion
         ;;
+
     2)
-        echo -e "${BLUE}Обновляем ноду Hemi...${NC}"
-
-        # Проверка существования сервиса
-        if systemctl list-units --type=service | grep -q "hemi-main.service"; then
-            sudo systemctl stop hemi-main.service
-            sudo systemctl disable hemi-main.service
-            sudo rm /etc/systemd/system/hemi-main.service
-            sudo systemctl daemon-reload
-        else
-            echo -e "${BLUE}Сервис hemi-main.service не найден, продолжаем обновление.${NC}"
-        fi
-        sleep 1
-
-        # Удаление папки с бинарниками, содержащими "hemi" в названии
-        echo -e "${BLUE}Удаляем старые файлы ноды...${NC}"
-        rm -rf hemi-main
-
-        # Установка бинарника
-        echo -e "${BLUE}Загружаем бинарник Hemi...${NC}"
-        curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v1.0.0/heminetwork_v1.0.0_linux_amd64.tar.gz
-
-        # Создание директории и извлечение бинарника
-        mkdir -p hemi-main
-        tar --strip-components=1 -xzvf heminetwork_v1.0.0_linux_amd64.tar.gz -C hemi-main
-        cd hemi-main
-
-        # Создание tBTC кошелька
-        ./keygen -secp256k1 -json  > popm-address.json
-
-        # Вывод содержимого файла popm-address.json
-        echo -e "${RED}Сохраните эти данные в надежное место:${NC}"
-        cat popm-address.json
-        echo -e "${PURPLE}Ваш pubkey_hash — это ваш адрес, на который нужно отправить BTC.${NC}"
-
-        echo -e "${YELLOW}Введите ваш приватный ключ от кошелька:${NC} "
-        read PRIV_KEY
-        echo -e "${YELLOW}Укажите желаемый размер комиссии (рекомендуем 1-2):${NC} "
-        read FEE
-
-        echo "POPM_BTC_PRIVKEY=$PRIV_KEY" > popmd.env
-        echo "POPM_STATIC_FEE=$FEE" >> popmd.env
-        echo "POPM_BFG_URL=wss://pop.hemi.network/v1/ws/public" >> popmd.env
-        echo "POPM_BTC_CHAIN_NAME=mainnet" >> popmd.env
-        sleep 1
-
-        # Определяем имя текущего пользователя и его домашнюю директорию
-USERNAME=$(whoami)
-
-if [ "$USERNAME" == "root" ]; then
-    HOME_DIR="/root"
-else
-    HOME_DIR="/home/$USERNAME"
-fi
-
-# Создаем или обновляем файл сервиса с использованием определенного имени пользователя и домашней директории
-cat <<EOT | sudo tee /etc/systemd/system/hemi-main.service > /dev/null
-[Unit]
-Description=PopMD Main Service
-After=network.target
-
-[Service]
-User=$USERNAME
-EnvironmentFile=$HOME_DIR/hemi-main/popmd.env
-ExecStart=$HOME_DIR/hemi-main/popmd
-WorkingDirectory=$HOME_DIR/hemi-main/
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-        # Обновление сервисов и включение hemi
-        sudo systemctl daemon-reload
-        sudo systemctl restart systemd-journald
-        sudo systemctl enable hemi-main
-        sleep 1
-
-        # Запуск ноды
-        sudo systemctl start hemi-main
-
-        # Заключительный вывод
-        echo -e "${GREEN}Установка завершена и нода запущена!${NC}"
-
-        # Завершающий вывод
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-        echo "sudo journalctl -u hemi-main -f"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-        sleep 2
-        sudo journalctl -u hemi-main -f
+        echo -e "${GREEN}Установлена актуальная версия ноды.${NC}"
         ;;
+
     3)
-        echo -e "${YELLOW}Укажите новое значение комиссии (рекомендуем 1-2):${NC}"
-        read NEW_FEE
-
-        # Проверка, что введенное значение не меньше 1
-        if [ "$NEW_FEE" -ge 1 ]; then
-            # Обновляем значение комиссии в файле popmd.env
-            sed -i "s/^POPM_STATIC_FEE=.*/POPM_STATIC_FEE=$NEW_FEE/" $HOME/hemi-main/popmd.env
-            sleep 1
-
-            # Перезапуск сервиса Hemi
-            sudo systemctl restart hemi-main
-
-            echo -e "${GREEN}Значение комиссии успешно изменено!${NC}"
-
-            # Завершающий вывод
-            echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-            echo -e "${YELLOW}Команда для проверки логов:${NC}" 
-            echo "sudo journalctl -u hemi-main -f"
-            echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-            echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-            echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-        else
-            echo -e "${RED}Ошибка: комиссия должна быть не меньше 1!${NC}"
-        fi
-        sleep 2
-        sudo journalctl -u hemi-main -f
+        echo -e "${BLUE}Проверяем логи контейнера...${NC}"
+        docker logs -f nillion
         ;;
 
     4)
-        echo -e "${BLUE}Удаление ноды Hemi...${NC}"
+        echo -e "${YELLOW}Введите новый RPC:${NC}"
+        read NEW_RPC
 
-        # Остановка и удаление сервиса Hemi
-        sudo systemctl stop hemi-main.service
-        sudo systemctl disable hemi-main.service
-        sudo rm /etc/systemd/system/hemi-main.service
-        sudo systemctl daemon-reload
-        sleep 1
+        # Остановка и удаление контейнера
+        docker stop nillion
+        docker rm nillion
 
-        # Удаление папки с названием, содержащим "hemi"
-        echo -e "${BLUE}Удаляем файлы ноды Hemi...${NC}"
-        rm -rf hemi-main
-        
-        echo -e "${GREEN}Нода Hemi успешно удалена!${NC}"
+        # Запуск с новым RPC
+        docker run -d --name nillion -v $HOME/nillion/verifier:/var/tmp nillion/verifier:v1.0.1 verify --rpc-endpoint "$NEW_RPC"
 
-        # Завершающий вывод
+        # Заключительное сообщение
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${YELLOW}Команда для проверки логов контейнера:${NC}"
+        echo "docker logs -f nillion"
         echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
         echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
         echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 2
+
+        # Запуск логов
+        docker logs -f nillion
         ;;
+
     5)
-        sudo journalctl -u hemi-main -f
+        echo -e "${RED}Сохраните эти данные в надежное место!${NC}"
+        cat $HOME/nillion/verifier/credentials.json
         ;;
-        
+
+    6)
+        echo -e "${BLUE}Удаление ноды...${NC}"
+
+        # Остановка и удаление контейнера
+        docker stop nillion
+        docker rm nillion
+
+        # Удаление папки проекта
+        rm -rf $HOME/nillion
+
+        # Заключительное сообщение
+        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+        echo -e "${GREEN}Нода Nillion успешно удалена!${NC}"
+        echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
+        sleep 1
+        ;;
+
+    *)
+        echo -e "${RED}Неверный выбор. Пожалуйста, введите номер от 1 до 6.${NC}"
+        ;;
 esac
